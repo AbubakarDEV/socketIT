527de8dc1a9a4d48c2405f7afc3df6461855abb2
Abubakar <abub1156@gmail.com>
2020-11-21 18:47:28 +0500

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+// Can also be used with $(document).ready()
+jQuery(function($){
+
+	$('.flexslider').flexslider({
+	animation:"slide",
+	// autoplay: true,
+	// autoplaySpeed: 2000,
+	
+});	
+	
+	
+
+
+ 
+
+
+ 
+});
+			
---
+++ index.js
@@ -0,0 +1,21 @@
+ gsap.from('.navbar',{ duration: 2, ease: "bounce.out", y: -500 })
+ gsap.from('.navbar .nav-item',{ duration: 2,opacity:0,delay:2   })
+ gsap.from('.flexslider',{ duration: 2,opacity:0,delay:2 })
+gsap.from('.navbar .navbar-brand',{ duration: 2,opacity:0,delay:2 })
+
+// gsap.timeline({
+//     scrollTrigger:{
+//         trigger:"div",
+//         start:"center center",
+//         end:"bottom top",
+//         scrub:true,
+//         markers:true
+//     }
+// })
+
+// .to(".text",{scale:0})
+// .from(".flexslider",{x:innerWidth*-1})
+
+
+
+
